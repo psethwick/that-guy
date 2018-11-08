04b64d9df5af12b69cac203b63f53ba155560094
Seth Rider <seth.rider@gmail.com>
2018-11-08 21:55:20 +0000

    lol

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.alarms.onAlarm.addListener(f => {
-  fetch("https://api.stackexchange.com/2.2/questions?page=1&pagesize=10&order=desc&sort=creation&site=stackoverflow&filter=!C(o*VkSJu.8H6iV)T")
+  fetch("https://api.stackexchange.com/2.2/questions?page=1&pagesize=1&order=desc&sort=creation&site=stackoverflow&filter=!C(o*VkSJu.8H6iV)T")
     .then(r => r.json().then(d => {
       var question = d.items[0].title;
       var body = d.items[0].body.replace("<p>","");
