a3eaca38d09ea0fd5c5c378dc22674635de48096
Seth Rider <seth.rider@gmail.com>
2018-11-08 21:19:36 +0000

    progress

--- background.js
+++ background.js
@@ -1,17 +1,21 @@
 // https://api.stackexchange.com/2.2/questions?page=1&pagesize=10&order=desc&sort=creation&site=stackoverflow&filter=!)8aDKR.(yQrG6bS
-chrome.alarms.onAlarm.addListener(function (alarm) {
-  var opt = {
-    type: "basic",
-    title: "Hey",
-    message: "should I google it?",
-    iconUrl: "./peng.png"
-  };
-  console.log("Got an alarm!", alarm);
-  chrome.notifications.create(undefined, opt, function (id) {
-    console.log(id);
-  }
-  );
+chrome.alarms.onAlarm.addListener(f => {
+  fetch("https://api.stackexchange.com/2.2/questions?page=1&pagesize=1&order=desc&sort=creation&site=stackoverflow&filter=!)8aDKR.(yQrG6bS")
+    .then(r => r.json().then(d => {
+      var q = d.items[0].title;
+      console.log(q);
+      var opt = {
+        type: "basic",
+        title: "Hey",
+        message: q,
+        iconUrl: "./peng.png"
+      };
+      chrome.notifications.create(undefined, opt, function (id) {
+        console.log(id);
+      }
+      );
+    }));
 });
 chrome.runtime.onInstalled.addListener(function () {
-  chrome.alarms.create("that-guy", { delayInMinutes: 0.1, periodInMinutes: 0.2 });
+  chrome.alarms.create("that-guy", { delayInMinutes: 0.1, periodInMinutes: 2 });
 });
