a8d3cc0821c39090604d7839c2fb1e8fe3cf5cc1
Seth Rider <seth.rider@gmail.com>
2018-11-12 19:49:35 +0000

    welp

--- background.js
+++ background.js
@@ -1,17 +1,13 @@
 chrome.alarms.onAlarm.addListener(f => {
   fetch("https://api.stackexchange.com/2.2/questions?page=1&pagesize=1&order=desc&sort=creation&site=stackoverflow&filter=!C(o*VkSJu.8H6iV)T")
     .then(r => r.json().then(d => {
-      var question = d.items[0].title;
-      var body = d.items[0].body.replace("<p>","");
-      var opt = {
-        type: "basic",
-        title: question,
-        message: body,
-        iconUrl: "./peng.png"
-      };
-      chrome.notifications.create(undefined, opt, undefined);
+      const body = d.items[0].body;
+      const div = document.createElement("div");
+      div.innerHTML = body;
+      const text = div.innerText || "";
+      chrome.tts.speak(text);
     }));
 });
 chrome.runtime.onInstalled.addListener(f => {
-  chrome.alarms.create("that-guy", { delayInMinutes: 15, periodInMinutes: 10 });
+  chrome.alarms.create("that-guy", { delayInMinutes: 30, periodInMinutes: 15 });
 });
